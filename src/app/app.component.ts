b693272c5d0e8b7d190d5941ea0a28e6cbd0011d
Nikunj Chapadia <nchapadia@pandora.com>
2017-11-11 11:43:35 -0800

    implemented api endpoints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,27 @@ export class AppComponent implements OnInit, OnChanges {
   newsitems = [];
 
   constructor(private newsService: NewsService, router: Router) {
+    // Test only
+    // newsService.sources().then(
+    //   (data) => {
+    //     console.log('SOURCES', data);
+    //   }
+    // );
+
     newsService.news().then(
       (data) => {
         console.log('> > > [app.component.ts:19]', data);
         this.newsitems = data;
       }
     );
+
+    // Test only
+    // newsService.articleDetail().then(
+    //   (data) => {
+    //     console.log('ARTICLE DETAIL', data);
+    //   }
+    // );
+
   }
 
   ngOnInit() {
