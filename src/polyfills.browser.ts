7f917a50ec327884baa2a80c03a83b933ab08ead
Saad Mir <saadmir@gmail.com>
2017-11-11 02:50:13 -0800

    removed mean.io cruft

--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -36,15 +36,9 @@ import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
-if ('production' === ENV) {
-  // Production
-
-} else {
-
   // Development
   Error.stackTraceLimit = Infinity;
 
   /* tslint:disable no-var-requires */
   require('zone.js/dist/long-stack-trace-zone');
 
-}
